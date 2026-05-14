c6920c73eec81f549b1face63ce06b0e279ed905
ELVIS ALMEIDA CORDEIRO <elvis-cordeiro@hotmail.com>
2026-05-14 10:45:32 -0300

    Aulas de Java 14/05

---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author elvis.acordeiro
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String Nome = "Elvis";
+       float nota = 10;
+       System.out.println("Esse aluno " + Nome + nota);
+    }
+    
+}
