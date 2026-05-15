faa3d29c623c6db944fec38e17796d0de45beb3f
ELVIS ALMEIDA CORDEIRO <elvis.acordeiro@senacsp.edu.br>
2026-05-15 11:17:03 -0300

    Atualização do Projeto 15/05 - Encapsulamento

--- Orientação a objetos/src/javameupacote/Cliente.java
+++ Orientação a objetos/src/javameupacote/Cliente.java
@@ -7,7 +7,16 @@ package javameupacote;
  */
 public class Cliente {
     //Atributos
-    String nome;
+    private String nome = "Elvis";
     int cpf;
     int codigo;
-}
+    //Criando Metodo Para consulta e alteração do nome é o encapsulado "Private" 
+    public String consultaNome(){
+        return this.nome;
+    }
+    
+    public void alteraNome(String nome){
+        this.nome = nome;
+    }
+    }
+
---
+++ Orientação a objetos/src/javameupacote/TestaCliente.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javameupacote;
+
+/**
+ *
+ * @author elvis.acordeiro
+ */
+public class TestaCliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Instanciando e criado objeto privado para consulta
+        Cliente c = new Cliente();
+        c.alteraNome("Elvis-Mestre");
+        System.out.println(c.consultaNome());
+        
+        
+        
+        
+                   
+    }
+    
+}
